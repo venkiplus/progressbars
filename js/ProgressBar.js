544c9f6d13aed5d38aa832806d9f7ea0d02282ef
Venkatesh Mamidipalli <venki.sree008@gmail.com>
2017-01-18 18:49:17 +0530

    Moved code
    Created separate function constructor and moved to separate file

--- js/ProgressBar.js
+++ js/ProgressBar.js
@@ -1,3 +1,41 @@
-function greet(name) {
-  return 'Hello, ' + name + '!';
+function ProgressBar(initialValue, id){
+  this.value = parseInt(initialValue);
+  console.log(this.initialValue);
+  this.limit=0;
+  this.progress = (this.value/230)*100;
+  this.progressTo = function(initialValue,limit,id){
+    this.limit = limit;
+    this.value = parseInt(this.value) + parseInt(initialValue);
+    if(this.value<=0){
+      this.progress =0;
+      this.value=0;
+    }else{
+      this.progress = (this.value/230)*100;
+    }
+    if(this.value>this.limit){
+      this.template=this.getTemplate(true);
+    }else{
+      this.template=this.getTemplate(false);
+    }
+    document.getElementById('progressBar'+id).innerHTML=this.template;
+  }
+
+  this.getTemplate = function(exceeded,id){
+    var template='';
+    if(!exceeded){
+      template = "<div class='noload' id='progressBar"+id+"'>"+
+        "<span class='loadtext' id='loadspan'>"+this.progress.toFixed()+"%</span>"+
+        "<div class='load' id='loaddiv' style='width:"+this.progress.toFixed()+"%'>"+
+        "</div>"+
+      "</div>";
+    }else{
+        template = "<div class='noload' id='progressBar"+id+"'>"+
+        "<span class='loadtext' id='loadspan'>"+this.progress.toFixed()+"%</span>"+
+        "<div class='load' id='loaddiv' style='background:red'>"+
+        "</div>"+
+      "</div>";
+    }
+    return template;
+  }
+  this.template= this.getTemplate(false,id);
 }
--- js/app.js
+++ js/app.js
@@ -68,47 +68,5 @@
     request.send();
     return json;
   }
-
-  function ProgressBar(initialValue, id){
-    this.value = parseInt(initialValue);
-    console.log(this.initialValue);
-    this.limit=0;
-    this.progress = (this.value/230)*100;
-    this.progressTo = function(initialValue,limit,id){
-      this.limit = limit;
-      this.value = parseInt(this.value) + parseInt(initialValue);
-      if(this.value<=0){
-        this.progress =0;
-        this.value=0;
-      }else{
-        this.progress = (this.value/230)*100;
-      }
-      if(this.value>this.limit){
-        this.template=this.getTemplate(true);
-      }else{
-        this.template=this.getTemplate(false);
-      }
-      document.getElementById('progressBar'+id).innerHTML=this.template;
-    }
-
-    this.getTemplate = function(exceeded,id){
-      var template='';
-      if(!exceeded){
-        template = "<div class='noload' id='progressBar"+id+"'>"+
-          "<span class='loadtext' id='loadspan'>"+this.progress.toFixed()+"%</span>"+
-          "<div class='load' id='loaddiv' style='width:"+this.progress.toFixed()+"%'>"+
-          "</div>"+
-        "</div>";
-      }else{
-          template = "<div class='noload' id='progressBar"+id+"'>"+
-          "<span class='loadtext' id='loadspan'>"+this.progress.toFixed()+"%</span>"+
-          "<div class='load' id='loaddiv' style='background:red'>"+
-          "</div>"+
-        "</div>";
-      }
-      return template;
-    }
-    this.template= this.getTemplate(false,id);
-  }
   window.onload=init();
 }());
